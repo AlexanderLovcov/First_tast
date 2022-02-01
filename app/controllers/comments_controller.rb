03f85b82f78ea97a618837e050014f2a7dd390ad
Alexander.Lovcov <sasha.lovcov.98@gmail.com>
2022-02-01 15:46:54 +0300

    feat:

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,21 @@ class CommentsController < ArticlesController
   before_action :set_article
 
   def index
-    @comment = Comment.all
+    @comments = Comment.all
   end
 
   def create
+    ArticleAnalyzer.get_comments(@article)
 
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to article_url(@article) }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :articles do
     member do
       get 'analyze', to: 'articles#count_comments'
+      get 'get_comments', to: "comments#create"
     end
     resources :comments, only: [:index]
+    #get 'get_comments', to: "comments#create"
   end
 
 end
